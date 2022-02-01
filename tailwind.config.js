d09faac3c39ca635d5615988ce222a1df1deef9d
Thiago <contatocostathiago@gmail.com>
2022-02-01 14:20:09 -0300

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    colors: {
+      primary: '#121212',
+      secondary: '#1C1C1C',
+      white: '#FFFFFF',
+      purple: '#514E7F',
+      purplehover: '#373556',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
