5ae849cf469f3f839579c30e7bb01ae15a182dac
Thiago Machado <contatocostathiago@gmail.com>
2022-03-18 19:28:22 -0300

    upgrade step 0 page

--- components/StartSubmission.tsx
+++ components/StartSubmission.tsx
@@ -13,29 +13,29 @@ export default function StartSubmission() {
       <TopBar />
 
       <div className="flex flex-col justify-center items-center mt-32 sm:mt-0">
-        <div className="mb-12">
-          <Image
-            src="/images/cobogo-block.svg"
-            width={84}
-            height={73}
-            alt="cobogo block"
-          />
-        </div>
-
-        <p className="text-white sm:text-xl sm:w-[618px] text-center mb-12">
+        <p className="text-white sm:text-2xl sm:w-[618px] text-center mb-8">
           to offer the best experience in{' '}
           <span className="font-bold">Web3 YouTube monetization</span>, we are
-          growing our community of Creators. Join our waitlist to be the first
-          in this revolutionary economy, and to earn{' '}
-          <span className="font-bold">100 CBG</span> tokens.
+          growing our community of Creators.
         </p>
+
+        <div className="sm:[572px] h-[130px] bg-secondary hidden sm:flex justify-center items-center p-4 mb-8">
+          <div className="flex mr-2">
+            <Image src="/images/100-cbg-icon.svg" width={108} height={108} />
+          </div>
+
+          <p className="text-white">
+            join our waitlist to be the first in this revolutionary economy, and
+            to earn <span className="font-bold">100 CBG</span> tokens.
+          </p>
+        </div>
 
         <Link
           href={
             query.ref ? `/submit/connect?ref=${query.ref}` : `/submit/connect`
           }
         >
-          <a className="mb-28">
+          <a className="mb-12">
             <Button
               text="start submission"
               color="bg-purple"
