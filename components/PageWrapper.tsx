12953fe3a648e51bdf00cd9531938bcd1357bade
Thiago Machado <contatocostathiago@gmail.com>
2022-03-24 20:54:50 -0300

    fixing PageWrapper

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default function PageWrapper({ children }) {
   return (
-    <div className="grid grid-rows-1 sm:grid-rows-[1520px_70px] md:grid-rows-[1265px_70px] lg:grid-rows-[930px_70px] xl:grid-rows-[780px_70px] 2xl:grid-rows-[1145px_70px] grid-cols-1 sm:grid-cols-[332px_1fr]">
+    <div className="grid grid-rows-1 sm:grid-rows-[1080px_70px] grid-cols-1 sm:grid-cols-[332px_1fr]">
       {children}
     </div>
   );
