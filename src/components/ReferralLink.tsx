3ff34fe5cc4fc04c3ab185ac66b9fa7472a1dfe2
Thiago Machado <contatocostathiago@gmail.com>
2022-03-29 21:51:59 -0300

    add feedback when copying

--- src/components/ReferralLink.tsx
+++ src/components/ReferralLink.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import { useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { IoCopySharp } from 'react-icons/io5';
 
@@ -6,6 +8,12 @@ interface ReferralLinkProps {
 }
 
 export default function ReferralLink({ referralCode }: ReferralLinkProps) {
+  const [copied, setCopied] = useState(false);
+
+  function handleSetCopy() {
+    setCopied(true);
+  }
+
   return (
     <div className="flex justify-center mb-8">
       <div className="px-4 h-[90px] bg-secondary flex justify-center items-center border-[1.5px] border-details">
@@ -18,12 +26,22 @@ export default function ReferralLink({ referralCode }: ReferralLinkProps) {
         </div>
 
         <div
+          onClick={handleSetCopy}
           className={`pr-4 h-[50px] bg-black border-[1.5px] border-l-0 border-details outline-none flex justify-center items-center`}
         >
           <CopyToClipboard
             text={`https://app.cobogo.social/submit?ref=${referralCode}`}
           >
-            <IoCopySharp className="hover:cursor-pointer" color="white" />
+            {copied ? (
+              <Image
+                src="/images/success-icon.svg"
+                width={11.5}
+                height={11.5}
+                alt="error icon"
+              />
+            ) : (
+              <IoCopySharp className="hover:cursor-pointer" color="white" />
+            )}
           </CopyToClipboard>
         </div>
       </div>
