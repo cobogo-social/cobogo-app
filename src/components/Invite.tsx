fcc7c79ef2d6cf7fa1339e40eea5acb670c70d53
Thiago Machado <contatocostathiago@gmail.com>
2022-03-30 13:01:48 -0300

    update text in share links

--- src/components/Invite.tsx
+++ src/components/Invite.tsx
@@ -58,7 +58,7 @@ export default function Invite({
                 </div>
 
                 <a
-                  href={`https://t.me/share/url?url=app.cobogo.social/submit?ref=${referralCode}&text=Hi! cobogo is a dapp that helps YouTubers fund sustainably using blockchain. Use my referral link when you sign up for free for the waitlist, and we both get rewards!`}
+                  href={`https://t.me/share/url?url=app.cobogo.social/submit?ref=${referralCode}&text=Hi! cobogo is a dapp that helps YouTubers fund themselves sustainably through their communities using blockchain. Use my referral link when you sign up for free for the waitlist, and we both get rewards!`}
                   className="text-white font-bold"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -78,7 +78,7 @@ export default function Invite({
                 </div>
 
                 <a
-                  href={`https://twitter.com/intent/tweet?text=Hi!%20cobogo%20is%20a%20dapp%20that%20helps%20YouTubers%20fund%20sustainably%20using%20blockchain.%20Use%20my%20referral%20link%20when%20you%20sign%20up%20for%20free%20for%20the%20waitlist,%20and%20we%20both%20get%20rewards!%0Aapp.cobogo.social/submit?ref=${referralCode}`}
+                  href={`https://twitter.com/intent/tweet?text=Hi!%20cobogo%20is%20a%20dapp%20that%20helps%20YouTubers%20fund%20themselves%20sustainably%20through%20their%20communities%20using%20blockchain.%20Use%20my%20referral%20link%20when%20you%20sign%20up%20for%20free%20for%20the%20waitlist,%20and%20we%20both%20get%20rewards!%0Aapp.cobogo.social/submit?ref=${referralCode}`}
                   className="text-white font-bold"
                   target="_blank"
                   rel="noopener noreferrer"
