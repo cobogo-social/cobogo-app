8a68b0f9a7dc02f3e0fa16be752174bfe7405e47
Rafael Lima <contato@rafael.adm.br>
2022-03-29 23:03:42 -0300

    NEXT_PUBLIC_COBOGO_API_URL -> COBOGO_API_URL

--- src/services/cobogoApi.ts
+++ src/services/cobogoApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_COBOGO_API_URL,
+  baseURL: process.env.COBOGO_API_URL,
   headers: {
     Authorization: `Bearer ${process.env.COBOGO_API_TOKEN}`,
   },
