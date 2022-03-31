c9d8c01764e0a3d0f65d1df1a9c9813a6a010d93
Thiago Machado <contatocostathiago@gmail.com>
2022-03-31 17:27:30 -0300

    blankslate page fix

--- src/components/CobogoTopBar.tsx
+++ src/components/CobogoTopBar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function CobogoTopBar() {
   return (
-    <div className="hidden sm:flex w-full justify-start items-center mb-[70px]">
+    <div className="flex w-full justify-start items-center mb-[70px]">
       <Image src="/images/logo.svg" width={120} height={27} />
     </div>
   );
