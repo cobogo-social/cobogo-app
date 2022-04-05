f52bb15974cb9f75b3d6b6070609d37ff1d128a1
Thiago Machado <contatocostathiago@gmail.com>
2022-04-05 12:07:28 -0300

    add restriction in start submission page

--- pages/submit/index.tsx
+++ pages/submit/index.tsx
@@ -3,6 +3,8 @@ import MobileMenu from '@components/MobileMenu';
 import PageWrapper from '@components/PageWrapper';
 import StartSubmission from '@components/StartSubmission';
 import Steps from '@components/Steps';
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
@@ -34,3 +36,20 @@ export default function Index() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (session?.user) {
+    return {
+      redirect: {
+        destination: '/submit/connect',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
