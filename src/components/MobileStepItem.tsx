e8dc05f7c715de0d82a5d0b81c28570e1f1f2e68
Thiago Machado <contatocostathiago@gmail.com>
2022-04-05 11:30:52 -0300

    MobileMenu fix

--- src/components/MobileStepItem.tsx
+++ src/components/MobileStepItem.tsx
@@ -5,7 +5,6 @@ interface StepItemProps {
   text: string;
   href: string;
   open?: boolean;
-  last?: boolean;
 }
 
 export default function MobileStepItem({
@@ -13,37 +12,22 @@ export default function MobileStepItem({
   text,
   href,
   open,
-  last,
 }: StepItemProps) {
   const { asPath } = useRouter();
 
   const selected = asPath === href ? 'border-blue' : 'border-details';
 
-  return last ? (
-    <div className="flex items-center text-white mb-6">
+  return (
+    <div className="flex items-center text-white mb-[13px]">
       <div
-        className={`border-4 sm:border-8 ${selected} w-8 sm:w-10 h-8 sm:h-10 flex items-center justify-center ${
-          open ? 'mr-4' : 'mr-0'
-        } sm:mr-6 text-xs sm:text-xl font-bold`}
+        className={`border-[6px] ${selected} w-[31px] h-[28px] flex items-center justify-center ${
+          open ? 'mr-[20px]' : 'mr-0'
+        } text-xs font-bold`}
       >
         {number}
       </div>
 
-      {open ? (
-        <p className="font-bold text-xs sm:text-base text-green">{text}</p>
-      ) : null}
-    </div>
-  ) : (
-    <div className="flex items-center text-white mb-6">
-      <div
-        className={`border-4 sm:border-8 ${selected} w-8 sm:w-10 h-8 sm:h-10 flex items-center justify-center ${
-          open ? 'mr-4' : 'mr-0'
-        } sm:mr-6 text-xs sm:text-xl font-bold`}
-      >
-        {number}
-      </div>
-
-      {open ? <p className="font-bold text-xs sm:text-base">{text}</p> : null}
+      {open ? <p className="font-bold">{text}</p> : null}
     </div>
   );
 }
