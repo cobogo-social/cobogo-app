4fcd79eaf8d5053e404c0c469a862f296bc2ed3d
Thiago Machado <contatocostathiago@gmail.com>
2022-04-28 12:53:56 -0300

    feat: add description meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,11 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
+
+          <meta
+            name="description"
+            content="cobogo is an aggregator of protocols that has the goal of funding Content Creators sustainably through their own community, using DeFi. It is a platform that leverages Web 3.0 to monetize Creators while maximizing their growth and community building abilities."
+          />
         </Head>
 
         <body>
