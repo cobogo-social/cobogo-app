0389d0e6d9b0587e199ace7714f1dcb3858cf1b9
Thiago Machado <contatocostathiago@gmail.com>
2022-04-29 13:22:25 -0300

    fix: adjust title on next-seo-config.js

--- next-seo-config.js
+++ next-seo-config.js
@@ -1,4 +1,4 @@
-const title = 'cobogo.social';
+const title = 'cobogo';
 const description =
   'cobogo is an aggregator of protocols that has the goal of funding Content Creators sustainably through their own community, using DeFi. It is a platform that leverages Web 3.0 to monetize Creators while maximizing their growth and community building abilities.';
 
