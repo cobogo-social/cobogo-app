0488dd316c2ab351551d71c857fd9f175c9d7845
Thiago Machado <contatocostathiago@gmail.com>
2022-04-29 12:58:38 -0300

    fix: add width 100% on ReferralDashboardChannelBox when have small display

--- src/components/ReferralDashboardChannelBox.tsx
+++ src/components/ReferralDashboardChannelBox.tsx
@@ -15,7 +15,7 @@ export default function ReferralDashboardChannelBox({
   status,
 }: ReferralDashboardChannelBoxProps) {
   return (
-    <div className="mb-[30px] w-[277px] h-[193px] bg-black border-[1.5px] border-gray5 flex flex-col justify-start">
+    <div className="mb-[30px] w-full sm:w-[277px] h-[193px] bg-black border-[1.5px] border-gray5 flex flex-col justify-start">
       {banner ? (
         <Image
           src={banner}
