f0bf036e4c0137f5c6851653aa7d06247574ad75
Thiago Machado <contatocostathiago@gmail.com>
2022-05-16 13:35:36 -0300

    feat: adds "back to home" button on TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,11 @@
+import Link from './Link';
+
 export default function TopBar() {
   return (
-    <div className="hidden sm:flex w-full justify-end items-center mb-[95px]" />
+    <div className="hidden sm:flex w-full justify-end items-center mb-[65px]">
+      <Link href="/">
+        <button className="font-bold">back to home</button>
+      </Link>
+    </div>
   );
 }
