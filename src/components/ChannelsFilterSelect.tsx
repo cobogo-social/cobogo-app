8dc643039ee602c0050d9551f772e413bfd87d13
Thiago Machado <contatocostathiago@gmail.com>
2022-05-27 12:10:03 -0300

    fix: ChannelsFilterSelect shadow

--- src/components/ChannelsFilterSelect.tsx
+++ src/components/ChannelsFilterSelect.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function ChannelsFilterSelect() {
   return (
-    <div className="flex">
+    <div className="flex h-[48px] shadow-[0_0px_4px_4px_rgba(0,0,0,0.25)]">
       <div className="w-[48px] h-[48px] border-[1.5px] bg-gray7 border-r-0 border-gray10 flex justify-center items-center">
         <Image
           src="/images/filter-icon.svg"
@@ -12,7 +12,7 @@ export default function ChannelsFilterSelect() {
         />
       </div>
 
-      <select className="w-[262px] sm:w-[223px] h-[48px] bg-gray7 border-[1.5px] border-l-0 border-gray10 mb-8 px-2 outline-none hover:cursor-pointer shadow-[0_0px_4px_4px_rgba(0,0,0,0.25)]">
+      <select className="w-[262px] sm:w-[223px] h-[48px] bg-gray7 border-[1.5px] border-l-0 border-gray10 mb-8 px-2 outline-none hover:cursor-pointer">
         <option value="most staked">most viewed</option>
         <option value="most staked">most recent</option>
         <option value="most staked">most staked</option>
