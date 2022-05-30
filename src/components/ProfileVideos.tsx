97388478a000e4032aa46286c6df2eb7af6855d6
Thiago Machado <contatocostathiago@gmail.com>
2022-05-30 18:41:51 -0300

    Revert "feat: new profile page with static media kit and static distributions"
    This reverts commit d57ab3888e39de66dc8c386537229d29ae04889c.

--- src/components/ProfileVideos.tsx
+++ src/components/ProfileVideos.tsx
@@ -3,12 +3,13 @@ import ProfileVideo from './ProfileVideo';
 interface ProfileVideosProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   videos: any[];
+  title: string;
 }
 
-export default function ProfileVideos({ videos }: ProfileVideosProps) {
+export default function ProfileVideos({ videos, title }: ProfileVideosProps) {
   return (
     <div className="hidden sm:flex flex-col w-full px-[147px] pb-[62px]">
-      <p className="text-[22px] mb-[26px]">latest videos</p>
+      <p className="text-[22px] mb-[26px]">{title}'s latest videos</p>
 
       <div className="flex">
         {videos.map((video) => (
