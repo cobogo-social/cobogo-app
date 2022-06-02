8bf57173d50461eae5cabc13ea7e1e07beb44719
Thiago Machado <contatocostathiago@gmail.com>
2022-06-02 11:45:05 -0300

    fix:  adds target="_blank" on "share on Twitter" anchor

--- src/components/Earn10CBGNotification.tsx
+++ src/components/Earn10CBGNotification.tsx
@@ -18,8 +18,10 @@ export default function Earn10CBGNotification({
         </p>
 
         <a
+          target="_blank"
           href={`https://twitter.com/intent/tweet?text=Hi!%20cobogo%20is%20a%20dapp%20that%20helps%20YouTubers%20fund%20themselves%20sustainably%20through%20their%20communities%20using%20blockchain.%20Use%20my%20referral%20link%20when%20you%20sign%20up%20for%20free%20for%20the%20waitlist,%20and%20we%20both%20get%20rewards!%0Aapp.cobogo.social/submit?ref=${referralCode}`}
           className="sm:w-[326px] font-bold text-xs sm:text-base text-blue flex"
+          rel="noreferrer"
         >
           share on Twitter
         </a>
