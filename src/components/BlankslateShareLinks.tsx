5d694200544a6e637ac6ac0c49fea6036d3cd314
Thiago Machado <contatocostathiago@gmail.com>
2022-06-06 12:30:42 -0300

    feat: adds new text on share links

--- src/components/BlankslateShareLinks.tsx
+++ src/components/BlankslateShareLinks.tsx
@@ -20,7 +20,11 @@ export default function BlankslateShareLinks({
         </div>
 
         <a
-          href={`https://t.me/share/url?url=app.cobogo.social/submit?ref=${referralCode}&text=Hi! cobogo is a dapp that helps YouTubers fund themselves sustainably through their communities using blockchain. Use my referral link when you sign up for free for the waitlist, and we both get rewards!`}
+          href={`https://t.me/share/url?url=app.cobogo.social/submit?ref=${referralCode}&text=Check this out!
+
+          cobogo is a dapp that helps YouTubers monetize themselves sustainably through their communities using blockchain.
+
+          Use my referral link when you sign up for free for the whitelist, and we both get rewards!`}
           className="font-bold sm:mr-[85px]"
           target="_blank"
           rel="noopener noreferrer"
@@ -40,7 +44,7 @@ export default function BlankslateShareLinks({
         </div>
 
         <a
-          href={`https://twitter.com/intent/tweet?text=Hi!%20cobogo%20is%20a%20dapp%20that%20helps%20YouTubers%20fund%20themselves%20sustainably%20through%20their%20communities%20using%20blockchain.%20Use%20my%20referral%20link%20when%20you%20sign%20up%20for%20free%20for%20the%20waitlist,%20and%20we%20both%20get%20rewards!%0Aapp.cobogo.social/submit?ref=${referralCode}`}
+          href={`https://twitter.com/intent/tweet?text=Check%20this%20out!%20%0A%0Acobogo%20is%20a%20dapp%20that%20helps%20YouTubers%20monetize%20themselves%20sustainably%20through%20their%20communities%20using%20blockchain.%20%0A%0AUse%20my%20referral%20link%20when%20you%20sign%20up%20for%20free%20for%20the%20whitelist,%20and%20we%20both%20get%20rewards!%0Aapp.cobogo.social/submit?ref=${referralCode}`}
           className="font-bold"
           target="_blank"
           rel="noopener noreferrer"
--- src/components/ShareLinks.tsx
+++ src/components/ShareLinks.tsx
@@ -8,7 +8,11 @@ export default function ShareLinks({ referralCode }: ShareLinksProps) {
   return (
     <div className="flex flex-col justify-center items-start">
       <a
-        href={`https://t.me/share/url?url=app.cobogo.social/submit?ref=${referralCode}&text=Hi! cobogo is a dapp that helps YouTubers fund themselves sustainably through their communities using blockchain. Use my referral link when you sign up for free for the waitlist, and we both get rewards!`}
+        href={`https://t.me/share/url?url=app.cobogo.social/submit?ref=${referralCode}&text=Check this out!
+
+        cobogo is a dapp that helps YouTubers monetize themselves sustainably through their communities using blockchain.
+
+        Use my referral link when you sign up for free for the whitelist, and we both get rewards!`}
         target="_blank"
         rel="noopener noreferrer"
       >
