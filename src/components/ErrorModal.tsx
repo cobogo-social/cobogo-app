5704542f0244567be89fd4966c93b4ec90e2ac73
Thiago Machado <contatocostathiago@gmail.com>
2022-06-08 14:59:05 -0300

    fix: shadows

--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -19,7 +19,7 @@ export default function ErrorModal() {
 
   return error ? (
     <div className="w-screen h-screen fixed top-0 right-0 z-10 flex justify-center items-center bg-black/[0.5]">
-      <div className="relative bg-primary w-[605px] min-h-[244px] flex flex-col justify-center items-center border-[1.5px] border-gray10 px-[70px] shadow-[0_0px_0px_10px_rgba(0,0,0,0.4)]">
+      <div className="relative bg-primary w-[605px] min-h-[244px] flex flex-col justify-center items-center border-[1.5px] border-gray10 px-[70px] shadow-[0_0px_4px_10px_rgba(0,0,0,0.4)]">
         <div className="flex flex-col items-start justify-center">
           <div
             onClick={closeModal}
