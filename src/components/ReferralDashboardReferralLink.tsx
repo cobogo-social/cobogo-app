3058f55e2dbb8ed1ac02a823f16e88e5277059ff
Thiago Machado <contatocostathiago@gmail.com>
2022-06-09 16:20:58 -0300

    fix: refactors ReferralDashboardReferralLink

--- src/components/ReferralDashboardReferralLink.tsx
+++ src/components/ReferralDashboardReferralLink.tsx
@@ -19,11 +19,13 @@ export default function ReferralDashboardReferralLink({
   }
 
   return (
-    <>
-      <div className="justify-center hidden mb-8 sm:flex">
-        <div className="px-4 h-[90px] bg-gray4 flex justify-center items-center">
-          <p className="mr-2 text-xs font-bold">your referral link</p>
+    <div className="justify-center mb-8 flex">
+      <div className="sm:px-4 h-[90px] sm:bg-gray4 flex flex-col sm:flex-row justify-center items-start sm:items-center">
+        <p className="mb-2 sm:mb-0 sm:mr-2 sm:text-xs font-bold">
+          your referral link
+        </p>
 
+        <div className="flex">
           <div
             className={`px-4 h-[50px] bg-black flex justify-center items-center border-[1.5px] ${
               currentWallet && 'border-r-0'
@@ -63,38 +65,6 @@ export default function ReferralDashboardReferralLink({
           )}
         </div>
       </div>
-
-      <div className="flex flex-col justify-center mb-8 sm:hidden">
-        <p className="mb-2 font-bold">your referral link</p>
-
-        <div className="flex">
-          <div className="px-4 h-[50px] bg-black flex justify-center items-center border-[1.5px] border-r-0 border-gray10">
-            <p className="text-xs font-bold text-blue sm:text-sm">
-              app.cobogo.social/submit?ref={referralCode}
-            </p>
-          </div>
-
-          <div
-            onClick={changeCopied}
-            className="pr-4 h-[50px] bg-black border-[1.5px] border-l-0 border-gray10 outline-none flex justify-center items-center"
-          >
-            <CopyToClipboard
-              text={`https://app.cobogo.social/submit?ref=${referralCode}`}
-            >
-              {copied ? (
-                <Image
-                  src="/images/success-icon.svg"
-                  width={11.5}
-                  height={11.5}
-                  alt="error icon"
-                />
-              ) : (
-                <IoCopySharp className="hover:cursor-pointer" color="white" />
-              )}
-            </CopyToClipboard>
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
