91a672d2e394a7757498270b637405ca724f2bb3
Thiago Machado <contatocostathiago@gmail.com>
2022-06-16 16:33:21 -0300

    feat: adds static single fold to media kit social

--- src/components/ProfileMediaKitSocial.tsx
+++ src/components/ProfileMediaKitSocial.tsx
@@ -1,7 +1,116 @@
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function ProfileMediaKitSocial(): JSX.Element {
-  return (
+  const [singleOpen, setSingleOpen] = useState<boolean>(false);
+  const [step, setStep] = useState<number>(1);
+
+  function openStep(stepNumber: number) {
+    setSingleOpen(true);
+    setStep(stepNumber);
+  }
+
+  function closeStep() {
+    setSingleOpen(false);
+  }
+
+  function skipStep() {
+    if (step < 6) {
+      setStep((c) => c + 1);
+    }
+  }
+
+  function backStep() {
+    if (step > 1) {
+      setStep((c) => c - 1);
+    }
+  }
+
+  return singleOpen ? (
+    <div className="flex w-full px-[150px] py-[70px] relative justify-between items-center">
+      <div className="flex absolute top-[74px] right-[152px]">
+        <div onClick={backStep} className="flex">
+          <Image
+            src="/images/back2-icon.svg"
+            width={16}
+            height={10}
+            alt="back icon"
+          />
+        </div>
+
+        <div onClick={skipStep} className="flex ml-[40px]">
+          <Image
+            src="/images/skip-icon.svg"
+            width={16}
+            height={10}
+            alt="skip icon"
+          />
+        </div>
+
+        <div onClick={closeStep} className="flex ml-[65px]">
+          <Image
+            src="/images/x2-icon.svg"
+            width={16}
+            height={15}
+            alt="x icon"
+          />
+        </div>
+      </div>
+
+      <div className="flex w-full">
+        <div className="flex flex-col w-full">
+          <div className="flex w-full">
+            <div className="mr-[20px] flex">
+              <Image
+                src="/images/ytb-icon.svg"
+                width={60}
+                height={42}
+                alt="youtube icon"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <p className="font-bold text-[22px]">YouTube</p>
+
+              <p className="font-bold text-blue">visit channel</p>
+            </div>
+          </div>
+
+          <div className="flex justify-around items-center w-full">
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-[60px] font-bold">3.87M</p>
+
+              <p className="text-[18px] font-bold text-gray6">subscribers</p>
+
+              <p className="text-[60px] font-bold">3.87M</p>
+
+              <p className="text-[18px] font-bold text-gray6">subscribers</p>
+            </div>
+
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-[60px] font-bold">3.87M</p>
+
+              <p className="text-[18px] font-bold text-gray6">subscribers</p>
+
+              <p className="text-[60px] font-bold">3.87M</p>
+
+              <p className="text-[18px] font-bold text-gray6">subscribers</p>
+            </div>
+
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-[60px] font-bold">3.87M</p>
+
+              <p className="text-[18px] font-bold text-gray6">subscribers</p>
+
+              <p className="text-[60px] font-bold">3.87M</p>
+
+              <p className="text-[18px] font-bold text-gray6">subscribers</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
     <div className="flex w-full px-[150px] py-[70px] relative justify-between items-center">
       <div className="flex hover:cursor-pointer absolute top-[30px] left-[30px]">
         <Image
@@ -14,7 +123,7 @@ export default function ProfileMediaKitSocial(): JSX.Element {
 
       <div className="flex flex-col">
         <div className="flex mb-[90px]">
-          <div className="mr-[27px] flex">
+          <div onClick={() => openStep(1)} className="mr-[27px] flex">
             <Image
               src="/images/ytb-icon.svg"
               width={74}
@@ -31,7 +140,7 @@ export default function ProfileMediaKitSocial(): JSX.Element {
         </div>
 
         <div className="flex">
-          <div className="mr-[27px] flex">
+          <div onClick={() => openStep(2)} className="mr-[27px] flex">
             <Image
               src="/images/tiktok-icon.svg"
               width={74}
@@ -50,7 +159,7 @@ export default function ProfileMediaKitSocial(): JSX.Element {
 
       <div className="flex flex-col">
         <div className="flex mb-[90px]">
-          <div className="mr-[27px] flex">
+          <div onClick={() => openStep(3)} className="mr-[27px] flex">
             <Image
               src="/images/instagram-icon.svg"
               width={74}
@@ -67,7 +176,7 @@ export default function ProfileMediaKitSocial(): JSX.Element {
         </div>
 
         <div className="flex">
-          <div className="mr-[27px] flex">
+          <div onClick={() => openStep(4)} className="mr-[27px] flex">
             <Image
               src="/images/twitter-icon.svg"
               width={74}
@@ -86,7 +195,7 @@ export default function ProfileMediaKitSocial(): JSX.Element {
 
       <div className="flex flex-col">
         <div className="flex mb-[90px]">
-          <div className="mr-[27px] flex">
+          <div onClick={() => openStep(5)} className="mr-[27px] flex">
             <Image
               src="/images/twitch-icon.svg"
               width={74}
@@ -103,7 +212,7 @@ export default function ProfileMediaKitSocial(): JSX.Element {
         </div>
 
         <div className="flex">
-          <div className="mr-[27px] flex">
+          <div onClick={() => openStep(6)} className="mr-[27px] flex">
             <Image
               src="/images/discord-icon.svg"
               width={74}
