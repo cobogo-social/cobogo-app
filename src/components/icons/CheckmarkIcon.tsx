19c87df6b015a1dfe3ff9ffca02e65755fd2de32
Thiago Machado <contatocostathiago@gmail.com>
2022-06-23 12:37:06 -0300

    feat: adds size prop on icon components

--- src/components/icons/CheckmarkIcon.tsx
+++ src/components/icons/CheckmarkIcon.tsx
@@ -1,9 +1,12 @@
-export default function CheckmarkIcon(): JSX.Element {
+interface CheckmarkIconProps {
+  size: number;
+}
+
+export default function CheckmarkIcon(props: CheckmarkIconProps): JSX.Element {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="22"
-      height="23"
+      width={props.size}
       fill="none"
       viewBox="0 0 22 23"
     >
