24945000b6797d73768f233d00c95f3824fab04f
Thiago Machado <contatocostathiago@gmail.com>
2022-06-29 13:31:48 -0300

    fix: add justify-start class on SidebarContainer component

--- src/components/containers/SidebarContainer.tsx
+++ src/components/containers/SidebarContainer.tsx
@@ -7,7 +7,7 @@ interface SidebarContainerProps {
 export default function SidebarContainer(props: SidebarContainerProps) {
   return (
     <div className="w-screen h-screen fixed top-0 right-0 z-[1000] bg-black/[0.5]">
-      <div className="absolute top-0 right-0 bg-primary w-[600px] h-screen flex flex-col justify-start items-center p-[70px] shadow-[0_0px_4px_10px_rgba(0,0,0,0.4)] overflow-auto">
+      <div className="absolute top-0 right-0 bg-primary w-[600px] h-screen flex flex-col justify-start items-start p-[70px] shadow-[0_0px_4px_10px_rgba(0,0,0,0.4)] overflow-auto">
         {props.children}
       </div>
     </div>
