fc61f92db11e6b5da7fccd8a46459522124bf2ac
Thiago Machado <contatocostathiago@gmail.com>
2022-07-15 13:55:13 -0300

    fix: removes process.env on next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ module.exports = {
       'yt3.ggpht.com',
       'i.ytimg.com',
       'lh3.googleusercontent.com',
-      process.env.NEXT_PUBLIC_AWS_BUCKET_URL,
       'cobogo-app.s3.sa-east-1.amazonaws.com',
     ],
   },
